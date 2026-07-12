9bcbbedb3864cb503107ee44a5f045e9caa46ad9
Psychovisual <zoeraw8@gmail.com>
2026-07-12 16:24:18 -0400

    updated main.py

--- main.py
+++ main.py
@@ -4,10 +4,7 @@ from WORK_ON_THIS_ONE import final_transcript
 
 RTTM_PATH = "audio.rttm"
 
-def run_interview(url):
-    print("downloading audio...")
-    dir, AUDIO_PATH = download.download_audio_only(url, save_to_disk=True)
-
+def process_audio(AUDIO_PATH):
     print("running diarization...")
     annotation = diarization.diarize_audio(AUDIO_PATH)
     with open(RTTM_PATH, "w") as rttm:
@@ -17,35 +14,35 @@ def run_interview(url):
     print("parsing...")
     onset_list, duration_list, speaker_list = diarization.parse_rttm(RTTM_PATH)
 
-    # this creates the timestamps we need to split the audio at to fit in whisper's 25mb limit
+    # timestamps to split the audio at to fit whisper's 25mb limit
     print("getting timestamps to split at...")
     timestamp_list = processing.get_split_timestamps(onset_list, duration_list)
 
-    # pre-process the audio w.r.t. the timestamps
     print("resampling and splitting audio...")
     segment_temp_dir, segment_temp_file_list = processing.segment_resample_audio(AUDIO_PATH, timestamp_list)
 
-    # this is fluff but it's harmless fluff
     print("processing filenames...")
     segment_file_list = whisper.combine_segment_filenames(segment_temp_dir, segment_temp_file_list)
 
-    # create the transcripts
     print("running segments through whisper...")
     temp_transcript_list = whisper.process_all_segments(segment_file_list)
 
-    #combine them
     print("combining transcripts...")
     full_transcription_list = whisper.combine_transcripts(timestamp_list, temp_transcript_list)
     with open("full_transcription.json", 'w') as file:
         json.dump(full_transcription_list, file, indent=2)
 
-    # combine the diarization
     combined_diarization_list = diarization.combine_authors(onset_list, duration_list, speaker_list)
 
-    # match the words up with the speakers to build the final transcript
     print("assembling final transcript...")
-    transcript = final_transcript(full_transcription_list, combined_diarization_list)
-    return transcript
+    return final_transcript(full_transcription_list, combined_diarization_list)
+
+
+def run_interview(url):
+    print("downloading audio...")
+    dir, AUDIO_PATH = download.download_audio_only(url, save_to_disk=True)
+    return process_audio(AUDIO_PATH)
+
 
 if __name__ == '__main__':
-    run_interview("https://www.youtube.com/watch?v=t9ufyuX4Cho", "hi", "hi2")
+    run_interview("https://www.youtube.com/watch?v=t9ufyuX4Cho")
